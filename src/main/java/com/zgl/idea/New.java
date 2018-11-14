14294b7f8b19f47a181aec03c48c8afb363a22ba
Gilgamesh2 <248650083@qq.com>
2018-11-14 22:53:52 +0800

    测试提交
    Signed-off-by: Gilgamesh2 <248650083@qq.com>

--- src/main/java/com/zgl/idea/New.java
+++ src/main/java/com/zgl/idea/New.java
@@ -3,19 +3,29 @@ package com.zgl.idea;
 
 import com.zgl.idea.dao.UserDao;
 import com.zgl.idea.dao.UserDaoImpl;
+import org.omg.CORBA.TIMEOUT;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import sun.net.www.http.HttpClient;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Properties;
 import java.util.Set;
+import java.util.concurrent.TimeoutException;
 
 public class New {
     public static void main(String[] args) {
 
-        ApplicationContext ctx=new ClassPathXmlApplicationContext("applicationContext.xml");
-        UserDaoImpl i=(UserDaoImpl)ctx.getBean("userDaoImpl");
-        System.out.println(i.get(12));
+        System.out.println(11);
+
 
     }
 
